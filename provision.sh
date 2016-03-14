5c4466800d50217a47b01cd2d46567a183d772e5
Mateusz Parzonka <parzonka@gmail.com>
2016-03-14 20:07:07 +0100

    Add docker-compose

--- provision.sh
+++ provision.sh
@@ -4,7 +4,10 @@
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 
-# docker
+# pip
+sudo apt-get -y install python-pip
+
+# docker with docker-compose
 sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 /etc/apt/sources.list.d/docker.list
@@ -14,8 +17,8 @@ sudo apt-cache policy docker-engine
 sudo apt-get install linux-image-extra-$(uname -r) -y
 sudo apt-get install apparmor -y
 sudo apt-get install docker-engine -y
+sudo pip install docker-compose
 sudo usermod -aG docker vagrant
 
 # awscli
-sudo apt-get -y install python-pip
 sudo pip install awscli
