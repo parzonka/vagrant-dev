3eb80b032ae24d15a2bb64122905ad5270479fa4
Mateusz Parzonka <parzonka@gmail.com>
2016-03-15 11:39:54 +0100

    Improve provisioning

--- provision.sh
+++ provision.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# update
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-
-# pip
-sudo apt-get -y install python-pip
-
 # docker with docker-compose
 sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-/etc/apt/sources.list.d/docker.list
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-cache policy docker-engine
 sudo apt-get install linux-image-extra-$(uname -r) -y
 sudo apt-get install apparmor -y
 sudo apt-get install docker-engine -y
+sudo apt-get -y install python-pip
 sudo pip install docker-compose
 sudo usermod -aG docker vagrant
 
